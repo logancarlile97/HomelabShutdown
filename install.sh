d27d8b24cd065427b2e18e9fd87bdaff65830fba
BrokenPillow <80006390+BrokenPillow@users.noreply.github.com>
2021-03-26 13:43:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ sudo -i -u pi bash << EOF
 touch /home/pi/HomelabShutdown/cronfile.txt
 /usr/bin/crontab -l > /home/pi/HomelabShutdown/cronfile.txt
 echo "@reboot sleep 5 && cd /home/pi/HomelabShutdown && python3 ./main.py" >> /home/pi/HomelabShutdown/cronfile.txt
-crontab /home/pi/HomelabShutdown/cronfile.txt.
+crontab /home/pi/HomelabShutdown/cronfile.txt
 rm /home/pi/HomelabShutdown/cronfile.txt
 EOF
