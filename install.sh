ae447d24a1a651e8d753ed2e6a922a7f7a375817
Logan Carlile <60319673+logancarlile97@users.noreply.github.com>
2021-03-26 17:15:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,9 @@ git clone https://github.com/logancarlile97/HomelabShutdown.git
 #Set up a cron job to auto start the program upon boot
 #Change to the pi user
 sudo -i -u pi bash << EOF
-#Create a file called cronfile.tmp
 touch /home/pi/HomelabShutdown/cronfile.tmp
-#Copy crontab contents to cronfile.tmp
 /usr/bin/crontab -l > /home/pi/HomelabShutdown/cronfile.tmp
-#Append the cron job to cronfile.tmp
 echo "@reboot sleep 5 && cd /home/pi/HomelabShutdown && python3 ./main.py" >> /home/pi/HomelabShutdown/cronfile.tmp
-#Write cronfile.tmp to the crontab
 crontab /home/pi/HomelabShutdown/cronfile.tmp
-#Delete the cronfile.tmp file
 rm /home/pi/HomelabShutdown/cronfile.tmp
 EOF
