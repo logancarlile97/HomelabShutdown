49fdd4d3b57959dee74c215670cb9992a241306d
BrokenPillow <80006390+BrokenPillow@users.noreply.github.com>
2021-03-26 13:23:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ apt install rpi.gpio -y
 pip3 install adafruit-circuitpython-charlcd -y
 
 #Set up the Cron tab job
-contab -l > cronfile.txt
+sudo su pi
+contab -l > /home/pi/HomelabShutdown/cronfile.txt
 echo "@reboot sleep 5 && cd /home/pi/HomelabShutdown && python3 ./main.py" >> /home/pi/HomelabShutdown/cronfile.txt
 crontab /home/pi/HomelabShutdown/cronfile.txt.
-rm cronfile.txt
+rm /home/pi/HomelabShutdown/cronfile.txt
