c06e80b76561c820f055f9c4e63cce78bb1fbd25
Logan Carlile <60319673+logancarlile97@users.noreply.github.com>
2021-03-28 19:01:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,13 +21,26 @@ sudo -i -u pi bash << EOF
 #Clone the githup repo
 git clone https://github.com/logancarlile97/HomelabShutdown.git
 
-#Create a cron job to run program upon boot
-touch /home/pi/HomelabShutdown/cronfile.tmp
-/usr/bin/crontab -l > /home/pi/HomelabShutdown/cronfile.tmp
-echo "@reboot sleep 5 && cd /home/pi/HomelabShutdown && python3 ./main.py" >> /home/pi/HomelabShutdown/cronfile.tmp
-crontab /home/pi/HomelabShutdown/cronfile.tmp
-rm /home/pi/HomelabShutdown/cronfile.tmp
+#Ask user if they want to create an autostart job for the program to start after boot
+echo 
+read -p 'Create a job to run program at boot? (y/n): ' crnJb <&1
+echo
+
+case $crnJb in
+  #Create a cron job to run program upon boot
+  y|Y )
+  touch /home/pi/HomelabShutdown/cronfile.tmp
+  /usr/bin/crontab -l > /home/pi/HomelabShutdown/cronfile.tmp
+  echo "@reboot sleep 5 && cd /home/pi/HomelabShutdown && python3 ./main.py" >> /home/pi/HomelabShutdown/cronfile.tmp
+  crontab /home/pi/HomelabShutdown/cronfile.tmp
+  rm /home/pi/HomelabShutdown/cronfile.tmp
+  ;;
+  
+  * ) echo "Skipped installing cron job" ;; 
 EOF
+
+#Delay to give user time to see previous message
+sleep 5
 
 #Ask user to reboot the pi to make the program run
 echo 
@@ -37,5 +50,6 @@ echo
 
 case $rbtNw in 
   y|Y ) echo "Rebooting in 10sec press Ctrl-c to cancel" && sleep 10 && reboot;;
+  
   * ) echo "Please reboot manualy later";;
 esac
